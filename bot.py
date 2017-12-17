3d836cd0da4c153ebba1df425115139e8f8f4503
banakin <banakin9@gmail.com>
2017-12-16 20:30:45 -0500

    Updated to version 1.1.0
    Faster and more efficient!

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
-# Version 1.0.0 beta  //  Made by Banakin for GashRTs
+# Version 1.1.0 beta  //  Made by Banakin for GashRTs
 # https://github.com/Banakin/GashBot
-
 
 import tweepy
 import json
-import time
 
 consumer_key = "TwitterAppConsumerKey"
 consumer_secret = "TwitterAppConsumerKeySecret"
@@ -12,25 +10,57 @@ access_token = "TwitterAppAccessToken"
 access_token_secret = "TwitterAppAccessTokenSecret"
 handle = "TwitterHandle" # Your twitter bot’s handle WITHOUT the @ sign
 
+print(''' &&&&&&\                      &&\              
+&&  __&&\                     && |             
+&& /  \__| &&&&&&\   &&&&&&&\ &&&&&&&\         
+&& |&&&&\  \____&&\ &&  _____|&&  __&&\        
+&& |\_&& | &&&&&&& |\&&&&&&\  && |  && |       
+&& |  && |&&  __&& | \____&&\ && |  && |       
+\&&&&&&  |\&&&&&&& |&&&&&&&  |&& |  && |       
+ \______/  \_______|\_______/ \__|  \__|       
+                                               
+                                               
+                                               
+                  &&&&&&&\             &&\     
+                  &&  __&&\            && |    
+                  && |  && | &&&&&&\ &&&&&&\   
+                  &&&&&&&\ |&&  __&&\\_&&  _|  
+                  &&  __&&\ && /  && | && |    
+                  && |  && |&& |  && | && |&&\ 
+                  &&&&&&&  |\&&&&&&  | \&&&&  |
+                  \_______/  \______/   \____/ 
+                                               
+                                               
+                                               
+Verson 1.1.0 beta // Made by Banakin // https://banakin.net''')
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-while 1 == 1:
-	search = api.search("@"+handle, count=15)
-	for tweet in search:
-		print('[Gash] Found the following tweet: '+str(tweet.id))
-		
-		for friendship in api.show_friendship(source_id=tweet.user.id, target_screen_name=handle):
-			if friendship.screen_name == handle:
-				if friendship.followed_by == True:
-					try:
-						print('[Gash] Retweeting...')
-						api.retweet(tweet.id)
-						print('[Gash] Retweeted!')
-					except:
-						print('[Gash] The tweet has been already retweeted, moving on...')
-				else:
-					print('[Gash] The poster of the tweet does not follow you, moving on...') # If you see this message twice it means that the bot found its own tweet, its ok.
-	time.sleep(45)
+class MyStreamListener(tweepy.StreamListener):
+	global handle
+	
+	def on_status(self, tweet):
+			global handle
+			
+			# The bots own retweets get found so it attemts to RT the RTs but it dosnt, its ok.
+			print('[Gash] Found the following tweet: '+str(tweet.id))
+			
+			for friendship in api.show_friendship(source_id=tweet.user.id, target_screen_name=handle):
+				if friendship.screen_name == handle:
+					if friendship.followed_by == True:
+						try:
+							print('[Gash] Retweeting...')
+							api.retweet(tweet.id)
+							print('[Gash] Retweeted!')
+						except:
+							print('[Gash] The tweet has been already retweeted, moving on...')
+					else:
+						print('[Gash] Not following you, moving on...') # If you see this message twice it means that the bot found its own tweet, its ok.
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
+
+myStream.filter(track=['@'+handle])
